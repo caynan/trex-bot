6f8fbd0154bfa93a1b2d346ec62a60a4b4e056c6
victorbc <vbatistacamilo@gmail.com>
2017-02-27 17:57:42 -0300

    Showing trex and obstacle info on screen by round

--- src/index.js
+++ src/index.js
@@ -4,13 +4,42 @@ import Game from './game'
 let game = new Game('#trex')
 let trex = game.getTRex()
 let obstacle = game.getNextObstacle()
+var count = 0;
+var table;
+var rowCount;
+var row;
 
 setInterval(() => {
-  game.updateState()
+    game.updateState()
 
-  if (trex.isActive()) {
-    console.log(trex.toString())
-    console.log(obstacle.toString())
-    trex.executeAction("DUCKING")
-  }
-}, 1000)
+    if (trex.isActive()) {
+        //console.log(trex.toString())
+        //console.log(obstacle.toString())
+
+        table = document.getElementById("info");
+        rowCount = table.rows.length;
+        count++;
+        if (trex.status == "CRASHED") {
+            count = 0;
+            for (var i = rowCount - 1; i > 0; i--) {
+                table.deleteRow(i);
+            }
+        }
+        if (rowCount >= 10) {
+            for (var i = 9; i > 0; i--) {
+                table.deleteRow(i);
+            }
+        }
+        if (count != 0) {
+            row = table.insertRow(rowCount);
+            row.insertCell(0).innerHTML = count;
+            row.insertCell(1).innerHTML = trex.status;
+            row.insertCell(2).innerHTML = (trex.speed + "").substring(0, 5);
+            row.insertCell(3).innerHTML = "x: " + trex.xPos + " , " + "y: " + trex.yPos;
+            row.insertCell(4).innerHTML = obstacle.width;
+            row.insertCell(5).innerHTML = "x: " + obstacle.xPos + " , " + "y: " + obstacle.yPos;
+        }
+
+        trex.executeAction("DUCKING")
+    }
+}, 500)
